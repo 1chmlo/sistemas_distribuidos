2b461f74bf2d3254abb5995ece68cf004aabdb46
camilo rios <camilo.rios1@mail.udp.cl>
2025-06-30 20:07:09 -0400

    cron (es necesario crear el crontab en cada maquina)

---
+++ tarea_3/cron/cron_jobs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Ir a la raíz del proyecto (directorio padre de 'cron')
+ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+
+log() {
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
+}
+
+cd "$ROOT_DIR"
+log "Ejecutando pipeline desde $ROOT_DIR"
+log "Ejecutando uploader"
+docker compose run --rm uploader
+log "Pipeline completado correctamente"
